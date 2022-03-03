02039f3c0420fd5662a05c559b2c1e0bb9eb6a76
Soundarya <soundarya.m.khanapur@gmail.com>
2022-03-03 18:35:50 +0530

    meal iteam add button

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,10 @@
+import Styles from './MealItemForm.module.css';
+
+const MealItemForm = props => {
+    return <form className={Styles.form}>
+        <input />
+        <button>+ Add</button>
+    </form>
+}
+
+export default MealItemForm;
